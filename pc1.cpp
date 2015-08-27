fbe78e5625d3ce2fec28d3d079fba29f129678ec
bbond <bbond002@student.butte.edu>
2015-08-27 22:13:33 +0000

    Start

--- pc1.cpp
+++ pc1.cpp
@@ -17,16 +17,25 @@ int main (int argc, char* argv[]) {
 	// CODE HERE
 	//
 	// 1 declare a string variable named userName
+	string userName;
 	// 2 declare a char variable named programmedBefore
+	char programmedBefore;
 	//   and initialize programmedBefore to the value 'z'
+	programmedBefore = "z";
 	// 3 display a welcome message to standard output
+	cout << "Welcome";
 	// 4 prompt the user for a name
+	cout << "Please enter your name";
 	// 5 read in the name from standard input and store
 	//   in the variable userName
+	getline(cin, userName);
 	// 6 ask the user the yes/no question "Have you programmed 
 	//   in C++ before?"
+	cout << "Have you programmed in C++ before? (Y/N)";
 	// 7 read in the answer from standard input and store
 	//   in the variable programmedBefore
+	cin >> programmedBefore;
+	programmedBefore = toupper (programmedBefore);
 	
 	unittest(userName, programmedBefore);
 	
